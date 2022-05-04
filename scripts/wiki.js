a6d521ca7b136563b3e7e9581f53ce3067817191
unknown <talaveramorenovictor@gmail.com>
2022-05-04 20:39:05 +0200

    corregir tabs

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -1,13 +1,13 @@
 function abrirCatalogo(evento, catalogo) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(catalogo).style.display = "block";
-    evento.currentTarget.className += " active";
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(catalogo).style.display = "block";
+	evento.currentTarget.className += " active";
 }
