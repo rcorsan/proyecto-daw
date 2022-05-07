51f483427371fb220e6d9dad8a2ec1d5830fbc03
unknown <talaveramorenovictor@gmail.com>
2022-05-07 12:22:54 +0200

    organizar

---
+++ js/generar.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+
+	generarNav();
+
+	function generarNav(){
+		const paginas = [
+			{titulo:"Inicio",ref:"./"},
+			{titulo:"Catálogos",ref:"./wiki.html"},
+			{titulo:"Ayuda",ref:"./help.html"}
+		];
+		let titulo = document.title;
+		let output = "";
+		paginas.forEach(pagina => {
+			if (titulo != pagina.titulo) {
+				output += "<a href=\""  + pagina.ref + "\"";
+				if (pagina.titulo == "Ayuda") {
+					output += " target=\"_blank\"";
+				}
+				output += ">" + pagina.titulo + "</a>";
+			}
+		});
+		$("#topnav").html(output);
+	}
+
+});
