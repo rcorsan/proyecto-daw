34bf0ecf42a2235b5fe06103b0633517513a1968
DrakZxho <drakzxho@gmail.com>
2022-06-05 17:31:55 +0200

    gameInit

--- js/game.js
+++ js/game.js
@@ -2,12 +2,42 @@ let session={};
 $(document).ready(function () {
     $("#play-button").click(function (e) { 
         e.preventDefault();
-        console.log(sessionCompr());
         if(sessionCompr()){
             session = JSON.parse(localStorage.getItem('session'));
-            
+            (session.playing)?(elegirPartida()):(gameInit());
         }else{
             $(location).attr('href', "login");
         }
     });
 });
+
+function gameInit(){
+    $("#index-buttons").css("display","none");
+    session = JSON.parse(localStorage.getItem('session'));
+    if (session.playing){
+        console.log("Continuando partida");
+    }else{
+        console.log("Nueva partida");
+        session.playing = true;
+    }
+    localStorage.setItem('session', JSON.stringify(session));
+}
+
+function elegirPartida(){
+    let output = "<h1 class='centered-title'>Ya existe una partida empezada.<br/>¿Deseas continuar la partida anterior?</h1>";
+    output += "<button id='continue-button' class='caja'>CONTINUAR PARTIDA</button>";
+    output += "<button id='new-game-button' class='caja'>NUEVA PARTIDA</button>";
+    $("#index-buttons").html(output);
+
+    $("#continue-button").click(function (e) { 
+        e.preventDefault();
+        gameInit();
+    });
+
+    $("#new-game-button").click(function (e) { 
+        e.preventDefault();
+        session.playing = false;
+        localStorage.setItem('session', JSON.stringify(session));
+        gameInit();
+    });
+}
