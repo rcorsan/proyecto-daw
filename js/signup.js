1a88b0df99aa90bc17f3dc6d1392391350b17804
unknown <raquelcorporales@gmail.com>
2022-06-12 19:01:47 +0200

    formulario

--- js/signup.js
+++ js/signup.js
@@ -14,7 +14,10 @@ $(document).ready(function () {
             password: password,
             email: email
         };
-        //FUNCION PARA ENVIAR LOS DATOS DEL FORMULARIO A LA API
+
+        /*
+        *FUNCION PARA ENVIAR LOS DATOS DEL FORMULARIO A LA API
+        */
         async function signup() {
             //GUARDA LA RESPUESTA DEL SERVIDOR AL ENVIAR EL OBJETO EN UNA VARIABLE
             let response = await postSignup(objeto).then((data) => data);
@@ -43,8 +46,19 @@ $(document).ready(function () {
             $("#alerta").css("visibility", "visible");
             $("#alerta").html(error);
             $("form")[0].reset();
-        } else {
-            //SI LAS CONTRASEÑAS COINCIDEN LLAMA A LA FUNCION 
+        } else if(password==""|password2==""|password==""&password2==""|email==""|email==""&password==""|email==""&password2==""|email==""&password==""&password2==""|user==""|user==""&password==""|user==""&password2==""|user==""&email==""|user==""&email==""&password==""|user==""&email==""&password2==""|user==""&email==""&password==""&password2==""){
+            let error = "Se requiere escribir todos los campos";
+            $("#alerta").css("visibility", "visible");
+            $("#alerta").html(error);
+            $("form")[0].reset();
+        }else if(!validarEmail(email)){
+            let error = "El correo no es válido";
+            $("#alerta").css("visibility", "visible");
+            $("#alerta").html(error);
+            $("form")[0].reset();
+        }
+         else{
+            //SI TODO ES CORRECTO LLAMA A LA FUNCION 
             signup();
         }
     });
@@ -70,3 +84,7 @@ async function postSignup(params) {
         console.error(error);
     }
 }
+function validarEmail(email) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(email);
+};
